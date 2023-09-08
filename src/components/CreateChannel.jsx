2f20c09439b8b74d8f2066b445571a23d9f7638d
connorabernethy <connorabernethy@icloud.com>
2023-09-08 15:48:48 -0700

    Create Channel capability

--- src/components/CreateChannel.jsx
+++ src/components/CreateChannel.jsx
@@ -5,8 +5,6 @@ import { UserList } from './';
 import { CloseIcon } from 'stream-chat-react';
 
 const ChannelNameInput = ({ channelName = '', setChannelName }) => {
-  const { client, setActiveChannel } = useChatContext();
-  const [selectedUsers, setselectedUsers] = useState([client.userID || '']);
 
   const handleChange = (e) => {
     e.preventDefault();
@@ -24,7 +22,29 @@ const ChannelNameInput = ({ channelName = '', setChannelName }) => {
 }
 
 const CreateChannel = ({ createType, setIsCreating }) => {
+  const { client, setActiveChannel } = useChatContext();
+  const [selectedUsers, setSelectedUsers] = useState([client.userID || '']);
   const [channelName, setChannelName] = useState('');
+
+  const createChannel = async (e) => {
+    e.preventDefault();
+
+    try {
+      const newChannel = await client.channel(createType, channelName, { 
+        name: channelName,
+        members: selectedUsers
+       });
+
+       await newChannel.watch();
+
+       setChannelName('');
+       setIsCreating(false);
+       setSelectedUsers([client.userID]);
+       setActiveChannel(newChannel);
+    } catch(error) {
+      console.log(error);
+    }
+  }
   return (
     <div className='create-channel__container'>
       <div className='create-channel__header'>
@@ -33,6 +53,9 @@ const CreateChannel = ({ createType, setIsCreating }) => {
       </div>
       {createType === 'team' && <ChannelNameInput channelName={channelName} setChannelName={setChannelName} />}
       <UserList setSelectedUsers={setSelectedUsers}/>
+      <div className='create-channel__button-wrapper' onClick={createChannel}>
+        <p>{createType === 'team' ? 'Create Channel' : 'Create Message Group'}</p>
+      </div>
     </div>
   )
 }
