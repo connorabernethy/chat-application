4aa06af9ac330a9bb9317f50d2656b4f683fdef9
connorabernethy <connorabernethy@icloud.com>
2023-09-20 17:18:05 -0700

    EditChannel functionality

--- src/components/EditChannel.jsx
+++ src/components/EditChannel.jsx
@@ -25,6 +25,28 @@ const EditChannel = ({ setIsEditing }) => {
   const { channel } = useChatContext();
   const [channelName, setChannelName] = useState(channel?.data?.name);
   const [selectedUsers, setSelectedUsers] = useState([]);
+
+  const updateChannel = async (event) => {
+    event.preventDefault();
+
+    try {
+      const nameChanged = channelName !== (channel.data.name || channel.data.id);
+      if (nameChanged) {
+        await channel.update({ name: channelName }, {text: `Channel name changed to: ${channelName}`});
+      }
+  
+      if (selectedUsers.length) {
+        await channel.addMembers(selectedUsers);
+      }
+      
+      setChannelName(null);
+      setIsEditing(false);
+      setSelectedUsers([]);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className='edit-channel__container'>
       <div className='edit-channel__header'>
@@ -33,6 +55,9 @@ const EditChannel = ({ setIsEditing }) => {
       </div>
       <ChannelNameInput channelName={channelName} setChannelName={setChannelName}/>
       <UserList setSelectedUsers={setSelectedUsers}/>
+      <div className='edit-channel__button-wrapper' onClick={updateChannel}>
+        <p>Save Changes</p>
+      </div>
     </div>
   )
 }
